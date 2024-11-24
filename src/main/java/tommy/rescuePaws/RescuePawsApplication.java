fb5631ef630fc7142922d977905b66dfdac18697
ImTommyDev <tommy.devlpr@gmail.com>
2024-11-24 21:54:39 +0100

    Primer commit, creación de la app, dependencias necesarias (pom.xml)

---
+++ src/main/java/tommy/rescuePaws/RescuePawsApplication.java
@@ -0,0 +1,13 @@
+package tommy.rescuePaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescuePawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescuePawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tommy/rescuePaws/RescuePawsApplicationTests.java
@@ -0,0 +1,13 @@
+package tommy.rescuePaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescuePawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
