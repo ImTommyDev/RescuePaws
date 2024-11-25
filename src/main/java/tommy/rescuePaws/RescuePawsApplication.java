8d6f2d11c02d039d52e9d5358ed209a3849f56c7
ImTommyDev <tommy.devlpr@gmail.com>
2024-11-25 16:42:09 +0100

    Propiedades de la aplicación creadas

--- src/main/java/tommy/rescuePaws/RescuePawsApplication.java
+++ src/main/java/tommy/rescuePaws/RescuePawsApplication.java
@@ -3,6 +3,7 @@ package tommy.rescuePaws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Esta clase permite ejecutar nuestra aplicación de spring boot gracias a la siguiente anotación
 @SpringBootApplication
 public class RescuePawsApplication {
 
